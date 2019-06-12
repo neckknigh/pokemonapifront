32d5c4666c506367cfac4ff8bdb824881f7c79d8
Jhon Vargas <jhonlavigne07@gmail.com>
2019-06-11 23:15:59 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles/app.scss';
+import TestComponent from "./components/testcomponent/testcomponent";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <TestComponent />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/testcomponent/testcomponent.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import "./testcomponent.scss";
+
+export default class TestComponent extends Component<any, any> {
+    render() {
+        return (
+            <h1 className="main">
+                Welcome..
+            </h1>
+        )
+    }
+}

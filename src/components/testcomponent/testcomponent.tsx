047b6bd9fbaa7610748d07afde7cbeca2baaf93a
Jhon Vargas <jhonlavigne07@gmail.com>
2019-06-16 22:37:39 -0500

    Welcome message updated

--- src/components/testcomponent/testcomponent.tsx
+++ src/components/testcomponent/testcomponent.tsx
@@ -5,7 +5,7 @@ export default class TestComponent extends Component<any, any> {
     render() {
         return (
             <h1 className="main">
-                Welcome..
+                The new Doomicilios website is being built with React technology
             </h1>
         )
     }

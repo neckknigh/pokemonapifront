f12b1d6e8f789730655fbf3b03e31c73b38e09ed
Jhon Lavigne <jhonlavigne07@gmail.com>
2019-08-06 22:59:05 -0500

    Se crea la sección de Tags

--- src/components/comunities/comunities.component.tsx
+++ src/components/comunities/comunities.component.tsx
@@ -2,13 +2,20 @@ import React, { Component } from 'react';
 import "./comunities.component.scss";
 import PopularComunities from './popular/popular-comunities.component';
 import RecomendedComunities from './recomended/recomended-comunities.component';
+import TagsFilterComponent from '../widgets/tags-filter/tags-filter.component';
 
 class Comunities extends Component<{}, {}> {
     public render() {
         return (
             <div className="comunities-container">
 
-                <PopularComunities />
+                <div className="">
+                    <PopularComunities />
+                </div>
+
+                <div className="tags-container">
+                    <TagsFilterComponent />
+                </div>
 
                 <RecomendedComunities />
             </div>

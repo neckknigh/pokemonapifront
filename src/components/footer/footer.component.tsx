587da11baeea0ed06613d8adfceaa2d48bc2fbdc
Jhon Lavigne <jhonlavigne07@gmail.com>
2019-08-08 22:54:42 -0500

    Footer: v1.1

--- src/components/footer/footer.component.tsx
+++ src/components/footer/footer.component.tsx
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
+import { IAppState } from '../../redux/app-state';
+import { connect } from 'react-redux';
 
-export default class FooterComponent extends Component<{}, {}> {
+interface FooterComponentProps {
+    readonly userHasSession: boolean;
+}
+
+class FooterComponent extends Component<FooterComponentProps, {}> {
     public render(): JSX.Element {
         return (
             <footer>
-                FooterComponent
+                {
+                    this.props.userHasSession &&
+                    <div>
+                        <p>FooterComponent</p>
+                    </div>
+
+                }
             </footer>
         );
     }
 }
+
+const mapStateToProps = (state: IAppState): FooterComponentProps => {
+    return {
+        userHasSession: state.authState.userHasSession
+    }
+}
+
+export default connect(mapStateToProps)(FooterComponent);

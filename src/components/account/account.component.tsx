0dd32c35332c2f83523eaf6767b5a49c4752304d
Jhon Lavigne <jhonlavigne07@gmail.com>
2019-08-12 22:00:58 -0500

    AccountComponent v1

--- src/components/account/account.component.tsx
+++ src/components/account/account.component.tsx
@@ -1,12 +1,42 @@
 import React, { Component } from 'react';
 import "./account.component.scss";
+import Popup from "reactjs-popup";
+
 
 class AccountComponent extends Component<{}, {}> {
     public render(): JSX.Element {
         return (
-            <div className="account">
-                <img src="/img/login/flags/dummy.png" alt="login-img" className="account-image clickable" />
-            </div>
+            <Popup
+                trigger={
+                    //<button>Trigger</button>
+                    <div className="flex-row-center-items-center account clickable">
+                        <img
+                            src="/img/login/flags/dummy.png"
+                            alt="account"
+                            className="account-image"
+                        />
+                        <div className="flex-row-center-items-center account-box">
+                            <span className="account-display">CUENTA</span>
+                            <span className="flex-row-center-items-center account-arrow">
+                                <i className="fas fa-chevron-down" />
+                            </span>
+                        </div>
+                    </div>
+                }
+                position="bottom right"
+                offsetX={11}
+                on="hover"
+                //defaultOpen={true}
+                contentStyle={{
+                    width: "264px"
+                }}
+                open={true}
+            >
+                <div className="account-popup-container">
+                    Content here
+
+                </div>
+            </Popup>
         );
     }
 }

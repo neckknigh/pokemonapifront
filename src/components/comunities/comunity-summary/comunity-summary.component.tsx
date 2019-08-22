1b065cdcc6ec0e3b3364771b61938769ebb1e1be
Jhon Lavigne <jhonlavigne07@gmail.com>
2019-08-21 23:48:12 -0500

    ComunitySummary v2

--- src/components/comunities/comunity-summary/comunity-summary.component.tsx
+++ src/components/comunities/comunity-summary/comunity-summary.component.tsx
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import "./comunity-summary.component.scss";
 import { withRouter, RouteComponentProps } from 'react-router';
-import * as History from 'history';
+//@ts-ignore
+import { ResponsiveImage, ResponsiveImageSize } from 'react-responsive-image';
+import StarRatingComponent from 'react-star-rating-component';
 
 interface IComunitySummaryComponentProps extends RouteComponentProps {
     //readonly history?: History.History;
@@ -15,19 +17,66 @@ class ComunitySummaryComponent extends Component<IComunitySummaryComponentProps,
         return (
             <div className="comunity-summary">
                 <div className="top">
-                    <img
-                        src="https://picsum.photos/id/85/1400/300"
-                        alt="banner" className="banner" />
 
+                    {/* TOO: REVISAR ESTO*/}
+                    <ResponsiveImage className="logo">
+                        <ResponsiveImageSize
+                            default
+                            minWidth={0}
+
+                            path={'https://picsum.photos/800/150'}
+                            className="logo"
+                        />
+                        <ResponsiveImageSize
+                            minWidth={768}
+                            path={'https://picsum.photos/1000/225'}
+                            className="logo"
+                        />
+                        <ResponsiveImageSize
+                            minWidth={992}
+                            path={'https://picsum.photos/1200/225'}
+                            className="logo"
+                        />
+                        <ResponsiveImageSize
+                            minWidth={1200}
+                            path={'https://picsum.photos/2000/225'}
+                            className="logo"
+                        />
+                    </ResponsiveImage>
 
                     <div className="grid banner-summary-info">
-                        <section className="column-20 test-border">
+                        <section className="column-20 flex-row-center-items-center ">
                             <img src="https://picsum.photos/150/150" alt="logo" className="logo" />
                         </section>
-                        <section className="column test-border">
-                            Touch to go - Las mejores alitas del mundos
-                       </section>
+                        <section className="column info-details">
+                            <div className="flex-row-items-center">
+                                <h3 className="title">Touch to go - Las mejores alitas del mundos</h3>
+                                <button className="btn-transparent favorite flex-row-items-center">
+                                    <i className="fas fa-heart icon-favorite" />
+                                </button>
+                            </div>
+                            <div >
+                                <p className="category">Restaurantes</p>
+                            </div>
+                            <div className="ratings">
+                                <StarRatingComponent
+                                    name="rate1"
+                                    starCount={5}
+                                    value={3}
+                                    renderStarIcon={(index: number, value: number) => {
+                                        const icon = index <= value ? 'fas fa-star' : 'far fa-star';
+                                        return (
+
+                                            <span>
+                                                <i className={`${icon} star-icon`} />
+                                            </span>
+                                        );
+                                    }}
+                                />
+                            </div>
+                        </section>
                     </div>
+
                 </div>
             </div>
         );
